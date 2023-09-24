2b37462e30ae66af373f7802e0bde854ac78354c
TomasWP <tomasfonseca14@ua.pt>
2023-09-24 19:28:38 +0100

    Ficheiros MPEI Aula 1

---
+++ Aula 1/Ex5.m
@@ -0,0 +1,4 @@
+disp([num2str(calcularProbabilidade(0.5, 6, 15, 1e5)*100),'%']);
+disp([num2str(calcularProbabilidade(0.5, 6, 20, 1e5)*100),'%']);
+disp([num2str(calcularProbabilidade(0.5, 6, 40, 1e5)*100),'%']);
+disp([num2str(calcularProbabilidade(0.5, 6, 100, 1e5)*100),'%']);
