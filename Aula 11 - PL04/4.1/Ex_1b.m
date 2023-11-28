0b2736a407cf25457295d8fb8118ec72d49500f4
TomasWP <tomasfonseca14@ua.pt>
2023-11-28 15:57:00 +0000

    Ficheiros MPEI Aula 11

--- Aula 11 - PL04/4.1/Ex_1b.m
+++ Aula 11 - PL04/4.1/Ex_1b.m
@@ -4,4 +4,4 @@ prob_chars = load("prob_pt.txt");
 i_min = 6;
 i_max = 20;
 
-strings = randStringArray(N, i_min, i_max, chars, prob_chars);
+strings = randStringArray(N, i_min, i_max, chars, prob_chars)
