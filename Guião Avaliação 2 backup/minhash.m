17ed54b648321a698788eedead62f8b56ba5fc21
TomasWP <tomasfonseca14@ua.pt>
2023-12-21 16:00:13 +0000

    Ficheiros Guião de Avaliação MPEI

--- Guião Avaliação 2 backup/minhash.m
+++
@@ -1,6 +0,0 @@
-function hashValues = minhash(shingles, index)
-    hashValues = zeros(1, length(shingles));
-    for i = 1:length(shingles)
-        hashValues(i) = min(mod(find(strcmp(shingles, shingles{i})), index));
-    end
-end
--- Guião Avaliação 2 backup/string2hash.m
+++
@@ -1,6 +0,0 @@
-function hash = string2hash(str)
-    hash = 0;
-    for i = 1:length(str)
-        hash = mod(hash * 256 + double(str(i)), 2^32 - 1);
-    end
-end
